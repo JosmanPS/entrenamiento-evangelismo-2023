e3953270a25c14db55e2f4263a0a80cc4b8a4360
Josman Proudinat <jmps2812@gmail.com>
2023-11-04 12:22:42 -0600

    Quote

--- components/Quote.vue
+++ components/Quote.vue
@@ -1,3 +1,12 @@
 <template>
-    <section></section>
+  <section class="py-12 bg-gradient-to-b from-orange-600 to-yellow-600">
+    <div class="container mx-auto">
+      <div class="max-w-xl mx-auto">
+        <p class="text-2xl font-bold text-center italic text-white uppercase">
+          "Creemos que este es el tiempo del derramamiento más fuerte de su
+          Espiritu Santo en todo el mundo a traves de la iglesia"
+        </p>
+      </div>
+    </div>
+  </section>
 </template>
