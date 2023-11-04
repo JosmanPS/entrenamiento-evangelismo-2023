0178adb8f1e903a184c03c86f83ec7bd6304e265
Josman Proudinat <jmps2812@gmail.com>
2023-11-04 12:42:42 -0600

    SEO

--- app.vue
+++ app.vue
@@ -10,3 +10,20 @@
     <Footer />
   </main>
 </template>
+
+<script setup lang="ts">
+const title = 'Entrenamiento de Evangelismo Sobrenatural'
+const description =
+  'Capacitación de Evangelismo práctico y efectivo con Cristian Ledesma. 11 de Nov. Tegucigalpa.'
+const seoImageUrl =
+  'https://evangelismo.escueladeavivamiento.com/img/seo-img.jpg'
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogImage: seoImageUrl,
+  twitterCard: 'summary_large_image',
+})
+</script>
